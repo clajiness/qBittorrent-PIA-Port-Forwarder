e3132a7b088f5c7a9d78f74793586d543b4a3306
Chad Lajiness <clajiness@gmail.com>
2019-06-06 11:12:43 -0500

    Updated script to recycle client id every x days added logging

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ### A Ruby script for automatically setting qBittorrents listening port while connected to PIA VPN
 
-**Config**
+**Config**\
 Enter your QBT credentials and IP address, and then enter your PIA credentials.
 
-**Environment**
+**Environment**\
 This script was tested with Ruby 2.5.3.
 
-**Usage**
-Run the script with `ruby qbt_pia_port_forwarder.rb`. It will create a text file named `pia_client_id_file.txt` for storing your client ID. If you wish to force a port change, simply delete the text file and run the script. I recommend executing the script with cron or a similar service.
+**Usage**\
+Run the script with `ruby qbt_pia_port_forwarder.rb`. It will create a text file named `@pia_client_id_file.txt` for storing your client ID. If you wish to force a port change, simply delete the text file and run the script. I recommend executing the script with cron or a similar service.
--- qbt_pia_port_forwarder.rb
+++ qbt_pia_port_forwarder.rb
@@ -3,48 +3,56 @@ require 'net/http'
 require 'net/https'
 require 'securerandom'
 require 'socket'
+require 'yaml'
+require 'logger'
 
-# CONFIG
+@logger = Logger.new('qbt_pia.log', 10, 1024000)
 
 # add your qbt credentials and ip address here
 QBT_USERNAME = "".freeze
 QBT_PASSWORD = "".freeze
-QBT_ADDR = "".freeze
+QBT_ADDR = "".freeze # ex. http://10.0.1.48:8080
 
 # add your pia credentials here
 PIA_USERNAME = "".freeze
 PIA_PASSWORD = "".freeze
 PIA_LOCAL_IP = Socket.getifaddrs.detect {|intf| !intf.addr.nil? && intf.addr.ip? && !intf.addr.ipv6? && intf.name.include?("tun")}.addr.ip_address
 
-if File.exist?("pia_client_id_file.txt")
-  f = File.open("pia_client_id_file.txt", "r")
-  f.each_line do |line|
-    PIA_CLIENT_ID = line.strip
+# days until you get a new port
+DAYS_TO_KEEP_PORT = 3
+
+def create_new_client_id_file
+  @pia_client_id = SecureRandom.hex(32)
+  File.open("client_id.yml", "w") do |out|
+    client_id = {:client_id => @pia_client_id, :created => Time.now}
+    YAML.dump(client_id, out)
   end
-  f.close
+end
+
+if File.exist?("client_id.yml")
+  config = YAML.load_file("client_id.yml")
+  created = config[:created]
+
+  if (Time.now - created) > (60 * 60 * 24 * DAYS_TO_KEEP_PORT)
+    create_new_client_id_file
+  else
+    @pia_client_id = config[:client_id]
+  end
 else
-  PIA_CLIENT_ID = SecureRandom.hex(32)
-  f = File.open("pia_client_id_file.txt", "w")
-  f.puts PIA_CLIENT_ID
-  f.close
+  create_new_client_id_file
 end
 # ----------
 
 # PIA METHODS
 def port_forward_assignment
-  uri = URI("https://www.privateinternetaccess.com/vpninfo/port_forward_assignment?user=#{PIA_USERNAME}&pass=#{PIA_PASSWORD}&client_id=#{PIA_CLIENT_ID}&local_ip=#{PIA_LOCAL_IP}")
-
-  # Create client
+  uri = URI("https://www.privateinternetaccess.com/vpninfo/port_forward_assignment?user=#{PIA_USERNAME}&pass=#{PIA_PASSWORD}&client_id=#{@pia_client_id}&local_ip=#{PIA_LOCAL_IP}")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-
-  # Create Request
   req =  Net::HTTP::Post.new(uri)
-
-  # Fetch Request
   http.request(req)
 rescue StandardError => e
+  @logger.error("HTTP Request failed (#{e.message})")
   puts "HTTP Request failed (#{e.message})"
 end
 # ----------
@@ -52,69 +60,55 @@ end
 # QBITTORRENT METHODS
 def qbt_auth_login
   uri = URI("#{QBT_ADDR}/api/v2/auth/login?username=#{QBT_USERNAME}&password=#{QBT_PASSWORD}")
-
-  # Create client
   http = Net::HTTP.new(uri.host, uri.port)
-
-  # Create Request
   req =  Net::HTTP::Get.new(uri)
-
-  # Fetch Request
-   http.request(req)
+  http.request(req)
 rescue StandardError => e
+  @logger.error("HTTP Request failed (#{e.message})")
   puts "HTTP Request failed (#{e.message})"
 end
 
 def qbt_app_preferences(sid)
   uri = URI("#{QBT_ADDR}/api/v2/app/preferences")
-
-  # Create client
   http = Net::HTTP.new(uri.host, uri.port)
-
-  # Create Request
   req =  Net::HTTP::Get.new(uri)
-
-  # Add headers
   req.add_field "Cookie", sid
-
-  # Fetch Request
   http.request(req)
 rescue StandardError => e
+  @logger.error("HTTP Request failed (#{e.message})")
   puts "HTTP Request failed (#{e.message})"
 end
 
 def qbt_app_setPreferences(sid, pia_port)
   uri = URI("#{QBT_ADDR}/api/v2/app/setPreferences?json=%7B%22listen_port%22:%20#{pia_port}%7D")
-
-  # Create client
   http = Net::HTTP.new(uri.host, uri.port)
-
-  # Create Request
   req =  Net::HTTP::Get.new(uri)
-
-  # Add headers
   req.add_field "Cookie", sid
-
-  # Fetch Request
   http.request(req)
 rescue StandardError => e
+  @logger.error("HTTP Request failed (#{e.message})")
   puts "HTTP Request failed (#{e.message})"
 end
 # ----------
-
-# get port from pia
-pia_port = JSON.parse(port_forward_assignment.body)["port"]
-puts "pia port: #{pia_port}"
 
 # get sid from qbt
 sid = qbt_auth_login["set-cookie"].split(";")[0]
 
 # get existing port from qbt
 qbt_port = JSON.parse(qbt_app_preferences(sid).body)["listen_port"]
+@logger.info("current qbt port: #{qbt_port}")
 puts "current qbt port: #{qbt_port}"
+
+# get port from pia
+pia_port = JSON.parse(port_forward_assignment.body)["port"]
+@logger.info("pia port: #{pia_port}")
+puts "pia port: #{pia_port}"
 
 # set new port in qbt
 if pia_port != qbt_port
   response = qbt_app_setPreferences(sid, pia_port)
-  puts "qbt port changed! (response status: #{response.code}: #{response.message})"
+  @logger.info("qbt port changed to #{pia_port} (response status: #{response.code}: #{response.message})")
+  puts "qbt port changed to #{pia_port} (response status: #{response.code}: #{response.message})"
 end
+
+@logger.close
